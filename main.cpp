6fe8e46aeb5157634dae18ce7233c96b04c88c59
MariaSy19 <101586605+MariaSy19@users.noreply.github.com>
2023-08-06 20:12:02 +0300

    edit
    -user can input the item added

--- main.cpp
+++ main.cpp
@@ -4,20 +4,75 @@ using namespace std;
 int main()
 {
     //Sll
-    linkedList<int >link;
-    link.insertFirst(10);
-    link.insertLast(9);
-    link.insertFirst(33);
-    link.insertLast(44);
-    link.insertLast(87);
-    link.insertFirst(765);
-    link.insertAtPos(5,100);
-    link.print();
-    cout << "After Remove the first Node in the linked list: \n";
-    link.RemoveAtFirst();
-    link.print();
-    cout << "After Remove the last Node in the linked list: \n";
-    link.RemoveAtLast();
-    link.print();
+    linkedList<int> link ;
+    int sz , items , item, index;
+    cout << "what the size of list you want: \n";
+    cin >> sz;
+    
+    for (int i = 0; i < sz; ++i)
+    {
+        cout << "Input the value: \n";
+        cin >> items;
+        link.insertLast(items);
+    }
+    int choice ;
+
+        do {
+            cout << "----SLL----" << endl;
+            cout << "1.Insert At First. \n";
+            cout << "2.Insert At Last. \n";
+            cout << "3.Insert At position. \n";
+            cout << "4.Remove From First. \n";
+            cout << "5.Remove From last. \n";
+            cout << "6.Remove From Key. \n";
+            cout << "7.Exist.\n";
+            cout << "Input what you want: \n";
+            cin >> choice;
+            switch (choice) {
+                case 1:
+                    cout << "Input the item want to add in the linked list: \n";
+                    cin >> item;
+                    link.insertFirst(item);
+                    cout << "--Insert At First--\n";
+                    link.print();
+                    break;
+                case 2:
+                    cout << "Input the item want to add in the linked list: \n";
+                    cin >> item;
+                    link.insertLast(item);
+                    cout << "--Insert At Last--\n";
+                    link.print();
+                    break;
+                case 3:
+                    cout << "Input the item want to add in the linked list: \n";
+                    cin >> item;
+                    cout << "What the Index you want to add th item in: \n";
+                    cin >> index;
+                    link.insertAtPos(index, item);
+                    cout << "--Insert At Position--\n";
+                    link.print();
+                    break;
+                case 4:
+                    link.RemoveAtFirst();
+                    cout << "--Delete At First--\n";
+                    link.print();
+                    break;
+                case 5:
+                    link.RemoveAtLast();
+                    cout << "--Delete At Last--\n";
+                    link.print();
+                    break;
+                case 6:
+                    exit(0);
+                    break;
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+            }
+        } while (choice != 6);
+
+
+
+
     return 0;
 }
