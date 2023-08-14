7e906c4fb3901fea3b6126f05c18d6312b98ba0d
MariaSy19 <101586605+MariaSy19@users.noreply.github.com>
2023-08-14 13:39:07 +0300

    add functions in class BST
    -inOrder
    -PreOrder
    -postOrder
    -CountHeight
    -countLeaves
    -max
    -isEmpty
    -clear
    -nodeCount
    -destructor
    -constructor

--- Tree.h
+++ Tree.h
@@ -81,5 +81,198 @@ void Tree<T>::levelOrder(Tree::node *root)
     }
 }
 
+///Binary Search Tree
+template<typename T>
+class BST
+{
+public:
+    bool isEmpty();
+    bool search(const T&);
+    bool searchRec(const T&);
+    void insert(const T&);
+    void remove(const T&);
+    void inOrder_traversal();
+    void preOrder_traversal();
+    void postOrder_traversal();
+    int treeHeight();
+    int treeNodeCount();
+    int treeLeavesCount();
+    void cleatTree();
+    BST();
+    ~BST();
+
+private:
+    struct node
+    {
+        T item;
+        node * right;
+        node * left;
+    };
+    node *root;
+    void clear(node *&ptr);
+    void inOrder(node *ptr);
+    void preOrder(node *ptr);
+    void postOrder(node *ptr);
+    int height(node *ptr);
+    T max(T& x , T& y);
+    int nodeCount(node *ptr);
+    int leavesCount(node *ptr);
+    void deleteFromTree(node *&ptr);
+    bool searchRecPriv(node * , int );
+};
+
+template<typename T>
+bool BST<T>::isEmpty() {
+    return (root == nullptr);
+}
+
+template<typename T>
+BST<T>::BST()
+{
+    root = nullptr;
+}
+
+template<typename T>
+void BST<T>::inOrder_traversal()
+{
+    inOrder(root);
+}
+
+template<typename T>
+void BST<T>::preOrder_traversal()
+{
+    preOrder(root);
+}
+
+template<typename T>
+void BST<T>::postOrder_traversal()
+{
+    postOrder(root);
+}
+
+template<typename T>
+int BST<T>::treeHeight()
+{
+    return height(root);
+}
+
+template<typename T>
+int BST<T>::treeNodeCount() {
+    return nodeCount(root);
+}
+
+template<typename T>
+int BST<T>::treeLeavesCount() {
+    return leavesCount(root
+    );
+}
+
+template<typename T>
+void BST<T>::inOrder(BST::node *ptr) //left root right
+{
+    if (ptr != nullptr)
+    {
+        inOrder(ptr->left);
+        cout << ptr->item << " ";
+        inOrder(ptr->right);
+    }
+}
+
+template<typename T>
+void BST<T>::preOrder(BST::node *ptr) //root left right
+{
+    if (ptr != nullptr)
+    {
+        cout << ptr->item << " ";
+        preOrder(ptr->left);
+        preOrder(ptr->right);
+    }
+}
+
+template<typename T>
+void BST<T>::postOrder(BST::node *ptr)//left right root
+{
+    if (ptr != nullptr)
+    {
+        postOrder(ptr->left);
+        postOrder(ptr->right);
+        cout << ptr->item << " ";
+    }
+
+}
+
+template<typename T>
+void BST<T>::clear(BST::node *&ptr)
+{
+    if (ptr != nullptr)
+    {
+        clear(ptr->left); //delete node in left subtree
+        clear(ptr->right); //delete node in right subtree
+        delete ptr;
+        ptr = nullptr;
+    }
+}
+
+template<typename T>
+void BST<T>::cleatTree()
+{
+    clear(root);
+}
+
+template<typename T>
+BST<T>::~BST()
+{
+    clear(root);
+}
+
+template<typename T>
+int BST<T>::height(BST::node *ptr) {
+    if (ptr == nullptr)
+    {
+        return 0;
+    }else
+    {
+        //This line calculates the height of the current node's subtree by recursively computing the height of its left subtree (height(p->left)) and its right subtree (height(p->right)).
+        // The height of the subtree is the maximum of the heights of the left and right subtrees plus 1 (the "+1" accounts for the current node).
+        //This is a classic pattern for calculating the height of a binary tree.
+        return  1 + max(height(ptr->left), height(ptr->right));
+    }
+}
+
+template<typename T>
+T BST<T>::max(T &x, T &y) {
+    if (x >= y)
+    {
+        return x;
+    }else
+    {
+        return y;
+    }
+}
+
+template<typename T>
+int BST<T>::nodeCount(BST::node *ptr) {
+    if (ptr == nullptr)
+    {
+        return 0;
+    } else
+    {
+        // 1 is a current node
+        // nodeCount(ptr->left) count the node in the left subtree
+        //nodeCount(ptr->right) count the node in the right subtree
+        return 1 + nodeCount(ptr->left) + nodeCount(ptr->right);
+    }
+}
+
+template<typename T>
+int BST<T>::leavesCount(BST::node *ptr) {
+    return 0;
+}
+
+template<typename T>
+bool BST<T>::searchRecPriv(BST::node *, int) {
+    return false;
+}
+
 
 #endif //UNTITLED27_TREE_H
